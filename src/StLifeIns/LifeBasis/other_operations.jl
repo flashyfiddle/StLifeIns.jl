f420273936e7434b9dc2e51931a51944e31343ee
flashyfiddle <73690154+flashyfiddle@users.noreply.github.com>
2022-01-06 16:15:10 +0200

    fixed basis getindex/combine_bases

--- src/StLifeIns/LifeBasis/other_operations.jl
+++ src/StLifeIns/LifeBasis/other_operations.jl
@@ -3,21 +3,29 @@ function combine_bases(bases::StProductBasis...)::StProductBasis
     proj = bases[1].proj
     mortality = bases[1].mortality
     surrender_rates = bases[1].surrender_rates
+    cum_infl = bases[1].cum_infl
+    int_acc = bases[1].int_acc
+    v =bases[1].v
     for i in 2:length(bases)
         nsims += bases[i].nsims
         bases[i].proj != proj && error("Bases of different projections")
         mortality = vcat(mortality, bases[i].mortality)
         surrender_rates = vcat(surrender_rates, bases[i].surrender_rates)
+        cum_infl = vcat(cum_infl, bases[i].cum_infl)
+        int_acc = vcat(int_acc, bases[i].int_acc)
+        v = vcat(v, bases[i].v)
     end
-    return StProductBasis(nsims, proj, mortality, surrender_rates)
+    return StProductBasis(nsims, proj, mortality, surrender_rates, cum_infl,
+    int_acc, v)
 end
 
 
 function Base.getindex(basis::StProductBasis, i::Union{Int64, UnitRange})::StProductBasis
     if i isa Int64
-        i= i:i
+        i = i:i
     end
 
     return StProductBasis(length(i), basis.proj, basis.mortality[i, :],
-    basis.surrender_rates[i, :])
+    basis.surrender_rates[i, :], basis.cum_infl[i, :], basis.int_acc[i, :],
+    basis.v[i, :])
 end
