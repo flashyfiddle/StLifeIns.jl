8bcd29db3960350178e51bda7ff70972c39cc8e7
flashyfiddle <73690154+flashyfiddle@users.noreply.github.com>
2022-01-06 11:26:09 +0200

    fixed empty_mortality_forecast function

--- src/MortalityModels/forecast/mortality_forecast.jl
+++ src/MortalityModels/forecast/mortality_forecast.jl
@@ -2,17 +2,32 @@ MortalityForecasts = OrderedDict{Int8, Matrix{Float64}}
 
 
 function empty_mortality_forecast(mortmodel::Dict{Bool, GAPC}, skip_extra=true::Bool)::Dict{Bool, MortalityForecasts}
-    extra = ceil(Int8, YEAR_MON - last(mortmodel.t))-1
-    low_age = first(mortmodel[true].x)
+    if skip_extra
+        low_age = first(mortmodel[true].x)
+        uxd = OrderedDict{Int8, Matrix{Float64}}()
+        t = 1
+        for x in low_age:(MAX_AGE-1)
+            uxd[x] = Matrix{Float64}(undef, 0, t)
+            t += 1
+        end
+        return Dict(true=>uxd, false=>uxd)
 
-    uxd = OrderedDict{Int8, Matrix{Float64}}()
-    t = ifelse(skip_extra, 1, extra+1)
-    for x in low_age:(MAX_AGE-1)
-        uxd[x] = Matrix{Float64}(undef, 0, t)
-        t += 1
+    else
+        empty = Dict{Bool, OrderedDict{Int8, Matrix{Float64}}}()
+        for gender in [0, 1]
+            model = mortmodel[gender]
+            extra = ceil(Int8, YEAR_MON - last(model.t))-1
+            low_age = first(model.x)
+            uxd = OrderedDict{Int8, Matrix{Float64}}()
+            t = extra+1
+            for x in low_age:(MAX_AGE-1)
+                uxd[x] = Matrix{Float64}(undef, 0, t)
+                t += 1
+            end
+            empty[gender] = uxd
+        end
+        return empty
     end
-
-    return Dict(true=>uxd, false=>uxd)
 end
 
 
